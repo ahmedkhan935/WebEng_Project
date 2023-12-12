6e8e7eeaaa823ad4b2791a21ef56578539249f6b
Musa <musaharoon.2003@gmail.com>
2023-12-12 19:25:00 +0500

    classroom model

--- server/models/Degree.js
+++ server/models/Degree.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Course = require('./Course');
 
 const DegreeSchema = new mongoose.Schema({
     name: {
--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 //Basic server setup using node
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+const adminRoutes = require('./routes/adminRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
+
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/admin', adminRoutes);
+app.use('/student', studentRoutes);
+app.use('/teacher', teacherRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 const port = 3000;
-
-app.listen(port, () => {
-    console.log("Server is running, Listening on port " + port);
-});
+app.listen(port, () => console.log("Server is running, Listening on port " + port));
