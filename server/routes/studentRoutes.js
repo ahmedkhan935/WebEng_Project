c9089125ffe56f5b3e158176a171352391088eb4
Musa <musaharoon.2003@gmail.com>
2023-12-12 19:32:32 +0500

    student controller

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -4,3 +4,5 @@ const studentController = require('../controllers/studentController');
 router.get('/profile', studentController.getStudentProfile);
 router.get('/courses', studentController.getStudentCourses);
 router.get('/notifications', studentController.getStudentNotifications);
+
+module.exports = router;
