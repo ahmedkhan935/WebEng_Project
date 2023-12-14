b6fe5c349c5a6c05d3178c5e5813aa3debe645c0
Fatima <fb71459@gmail.com>
2023-12-15 01:04:20 +0500

    updated mainpage responsiveness

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -4,8 +4,11 @@ import mainPageImage from "../assets/images/MainPage.png";
 import cleanSlateImage from "../assets/images/Hat.png";
 import theme from "../assets/theme/theme";
 import { Link } from "react-router-dom";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const MainPage = () => {
+  const isScreenSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
   const styles = {
     container: {
       display: "flex",
@@ -17,21 +20,19 @@ const MainPage = () => {
       flexDirection: "column",
       alignItems: "center",
     },
-    button: {
-      margin: "10px",
-    },
+
     mainPageImage: {
       marginTop: "50px",
       width: "600px",
       height: "600px",
-      "object-fit": "contain",
+      objectFit: "contain",
+      display: isScreenSmall ? "none" : "block", // Hide on smaller screens
     },
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={0}>
-        {/* Combine the two sides without spacing */}
         <Grid item xs={12} sm={6} sx={styles.container}>
           <Paper elevation={0} sx={styles.loginPaper}>
             <Typography variant="h2" sx={{ fontWeight: "bold" }}>
@@ -39,13 +40,13 @@ const MainPage = () => {
                 src={cleanSlateImage}
                 alt="CleanSlate"
                 style={{ width: "100%" }}
-              ></img>
+              />
               CleanSlate
             </Typography>
             <Container
               sx={{
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: isScreenSmall ? "column" : "row", // Stack on smaller screens
                 marginBottom: "20px",
               }}
               style={{ marginTop: "50px", textDecoration: "none" }}
@@ -55,16 +56,25 @@ const MainPage = () => {
               <Button
                 variant="contained"
                 style={{
-                  marginLeft: "100px",
-                  marginRight: "50px",
+                  marginLeft: isScreenSmall ? "10" : "100px",
+                  marginRight: isScreenSmall ? "0" : "50px",
+                  marginBottom: isScreenSmall ? "10px" : "0", // Add margin bottom on smaller screens
                   background: theme.palette.secondary,
+                  width: isScreenSmall ? "100%" : "auto", // Full width on smaller screens
                 }}
                 component={Link}
                 to="/login"
               >
                 Login As Student
               </Button>
-              <Button variant="outlined">Login As Teacher</Button>
+              <Button
+                variant="outlined"
+                style={{
+                  width: isScreenSmall ? "100%" : "auto", // Full width on smaller screens
+                }}
+              >
+                Login As Teacher
+              </Button>
             </Container>
           </Paper>
         </Grid>
