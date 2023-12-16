381fce35b335cb5a9e17c2b26fb3ef8856dda64f
haadiyeah <pazocado@gmail.com>
2023-12-17 02:03:17 +0500

    Updated custom theme provider

--- client/src/assets/theme/CSThemeProvider.js
+++ client/src/assets/theme/CSThemeProvider.js
@@ -1,33 +1,33 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import useStore from "../../store/store"; //zustand store for darkmode
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useStore from '../../store/store'; //zustand store for darkmode
 
 export function CSThemeProvider({ children }) {
-  const { darkMode } = useStore();
+    const { darkMode } = useStore();
 
-  const theme = createTheme({
-    palette: {
-      mode: darkMode ? "dark" : "light",
-      primary: {
-        main: "#22717d",
-      },
-      secondary: {
-        main: "#de8a57",
-      },
-      background: {
-        default: darkMode ? "#333" : "#ebfafc",
-      },
-    },
-    typography: {
-      fontFamily: "Rethink Sans, sans-serif",
-      h5: {
-        fontWeight: "bold",
-        color: "#22717d", // primary color
-      },
-      h4: {
-        fontSize: "1.5rem",
-      },
-    },
-  });
+    const theme = createTheme({
+        palette: {
+            mode: darkMode ? 'dark' : 'light',
+            primary: {
+                main: '#22717d',
+            },
+            secondary: {
+                main: '#de8a57',
+            },
+            background: {
+                default: darkMode ? '#333' : '#ebfafc', 
+            }
+        },
+        typography: {
+            fontFamily: 'Rethink Sans, sans-serif',
+            h5: {
+                fontWeight: 'bold',
+                color: '#22717d', // primary color
+            },
+            h4: {
+                fontSize: '1.5rem',
+            }
+        },
+    });
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
