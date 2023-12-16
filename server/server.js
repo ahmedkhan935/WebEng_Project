c486a336839e8adad497ae35d6e9829d571d2949
ahmedkhan935 <ahmedkhanraza935@gmail.com>
2023-12-16 22:40:55 +0500

    added file uploads and downloads from firebase

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const bucket = require("./firebase_init");
+
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,33 +16,58 @@ mongoose.connection.on('connected', () => {
 
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(express.json());    
+app.use(fileUpload());
 app.use(cors({
   origin: 'http://localhost:5000', // specify the origin
   credentials: true // include credentials
 }));
+
 
 const adminRoutes = require('./routes/adminRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
 const authRouter = require('./routes/authRoutes');
 const threadRouter = require('./routes/threadRoutes');
-
-app.use(bodyParser.json());
-app.use(cors(
-    {
-        origin: "http://localhost:5000",
-        credentials: true
-    }
-));
 app.use('/admin', adminRoutes);
 app.use('/student', studentRoutes);
 app.use('/teacher', teacherRoutes);
 app.use('/auth', authRouter);
 app.use('/thread', threadRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/', async (req, res) => {
+    try {
+           
+        
+        if(!req.files) {
+            res.status(400).send('No file uploaded.');
+            return;
+        }
+        const file = req.files.xyz
+        ;
+        console.log(file);
+        if (!file) {
+            res.status(400).send('No file uploaded.');
+            return;
+        }
+        const blob = bucket.file(file.originalname);
+        const blobWriter = blob.createWriteStream({
+            metadata: {
+                contentType: file.mimetype,
+            },
+        });
+        blobWriter.on('error', (err) => next(err));
+        blobWriter.on('finish', () => {
+            // Assembling public URL for accessing the file via HTTP
+            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            // Return the file name and its public URL
+            res.status(200).send({ fileName: file.originalname, fileLocation: publicUrl });
+        });
+        blobWriter.end(file.buffer);
+    } catch (err) {
+        console.log(err.message);
+        res.json({ message: err });
+    }
 });
 
 const port = 3000;
