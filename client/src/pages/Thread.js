16fffa1c72cae280834a7207702bab40f884356c
haadiyeah <pazocado@gmail.com>
2023-12-23 16:55:51 +0500

    finally got the thread page to work

--- client/src/pages/Thread.js
+++ client/src/pages/Thread.js
@@ -2,10 +2,11 @@ import React, { useEffect } from "react";
 import { Box, Typography, Button, Container } from "@mui/material";
 import Announcement from "../components/AnnouncementCard";
 import NavBar from "../components/Navbar";
-import {getThread} from '../services/StudentService'
+import {getThread as getStudentThread} from '../services/StudentService'
+import {getThread as getTeacherThread} from '../services/TeacherService'
 import { useParams } from "react-router";
 import AnnouncementList from "../components/AnnouncementList";
-
+import { useLocation } from 'react-router-dom';
 
 function Thread() {
   const [thread, setThread] = React.useState();
@@ -13,20 +14,30 @@ function Thread() {
   const [postsFetched, setPostsFetched] = React.useState(false); //To check if classes have been fetched or not
   const threadId = useParams().id;
 
+  const location = useLocation();
+  const userRole = location.pathname.split('/')[1];
+
   useEffect(() => {
-    // console.log("use effect")
-    // console.log("Thread id", threadId)
-    getThread(threadId).then((data) => {
-      if (data.error) {
-        setPostsError(data.error);
-        setPostsFetched(true);
-        return;
-      }
-      setThread(data.data);
-      setPostsFetched(true);
-    });
+    if (userRole == "student") {
+      getStudentThread(threadId).then((data) => {
+        handleData(data);
+      });
+    } else if (userRole == "teacher") {
+      getTeacherThread(threadId).then((data) => {
+        handleData(data);
+      });
+    }
   }, []);
 
+  const handleData = (data) => {
+    if (data.error) {
+      setPostsError(data.error);
+      setPostsFetched(true);
+      return;
+    }
+    setThread(data.data);
+    setPostsFetched(true);
+  }
   return (
     <NavBar>
       <Box
