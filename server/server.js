7523843b49bc775cfdfa8a1c6fd6aafd04199070
ahmedkhan935 <ahmedkhanraza935@gmail.com>
2023-12-26 23:09:56 +0500

    cleanup

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ require("dotenv").config();
 
 mongoose
   .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
