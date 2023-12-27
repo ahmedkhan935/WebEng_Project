ae6100b3699c26d5f36df6a6db2d5bab4a830877
Fatima <fb71459@gmail.com>
2023-12-27 11:07:10 +0500

    minor changes in start semester

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -14,7 +14,12 @@ import {
   CardActions,
   CardMedia,
   CircularProgress,
+  alpha,
 } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useLocation } from "react-router-dom";
+import AnnouncementOutlined from "@mui/icons-material/AnnouncementOutlined";
 import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
 import Face2Icon from "@mui/icons-material/Face2";
@@ -32,6 +37,7 @@ import { endSemester, startSemester } from "../services/AdminService";
 
 const LandingPage = () => {
   const navigate = useNavigate();
+  const [ongoing, setOngoing] = useState("");
   const [semesterModalOpen, setSemesterModalOpen] = useState(false);
   const [semesterModalContent, setSemesterModalContent] = useState("");
   const [semesterModalIcon, setSemesterModalIcon] = useState(null);
@@ -84,22 +90,27 @@ const LandingPage = () => {
         setSemesterModalOpen(true);
       } else {
         console.log(res);
-        if (res.semester === "Fall") {
-          setShowfall(true);
-          setTimeout(() => {
-            setShowfall(false);
-          }, 5000);
+        if (res.success == true) {
+          if (res.semester === "Fall") {
+            setShowfall(true);
+            setTimeout(() => {
+              setShowfall(false);
+            }, 5000);
+          }
+          if (res.semester === "Spring") {
+            setShowspring(true);
+            setTimeout(() => {
+              setShowspring(false);
+            }, 5000);
+          }
+          setSemesterModalContent(res.message);
+          setSemesterModalIcon(icon);
+          setSemesterModalOpen(true);
+        } else {
+          setSemesterModalContent(res.message);
+          setSemesterModalIcon(icon);
+          setSemesterModalOpen(true);
         }
-        if (res.semester === "Spring") {
-          setShowspring(true);
-          setTimeout(() => {
-            setShowspring(false);
-          }, 5000);
-        }
-        var message = res.semester + " semester has started ";
-        setSemesterModalContent(message);
-        setSemesterModalIcon(icon);
-        setSemesterModalOpen(true);
       }
     } catch (error) {
       // Handle errors, e.g., display an error message
@@ -154,217 +165,229 @@ const LandingPage = () => {
   return (
     <NavBar>
       <Container>
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          pointerEvents: "none",
-        }}
-      >
-        {showfall && (
-          <img
-            src={autumn}
-            alt="Fall"
-            style={{ width: "100%", height: "100%", objectFit: "cover", zIndex: 2 }}
-          />
-        )}
-        {showspring && (
-          <img
-            src={spring}
-            alt="Spring"
-            style={{ width: "100%", height: "100%", objectFit: "cover",zIndex: 2  }}
-          />
-        )}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-end",
-        }}
-      >
-        <Box
-          display="flex"
-          flexDirection={{ xs: "column", md: "row" }}
-          alignItems="center"
-          justifyContent="flex-end"
-          gap={2}
-          sx={{ marginBottom: "20px" }}
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            pointerEvents: "none",
+          }}
         >
-          <Button
-            component={Link}
-            onClick={() => {
-              navigate("/admin/searchCourses");
-            }}
-            variant="outlined"
-            color="secondary"
-            startIcon={
-              <VisibilityOutlinedIcon
-                color="primary"
-                style={{ fontSize: 25 }}
-              />
-            }
-          >
-            View Courses
-          </Button>
-          <Button
-            component={Link}
-            onClick={() => {
-              navigate("/admin/viewStudents");
-            }}
-            variant="outlined"
-            color="secondary"
-            startIcon={<Face2Icon color="primary" style={{ fontSize: 25 }} />}
-          >
-            View Students
-          </Button>
-          <Button
-            component={Link}
-            onClick={() => {
-              navigate("/admin/viewTeachers");
-            }}
-            variant="outlined"
-            color="secondary"
-            startIcon={
-              <PersonSearchIcon color="primary" style={{ fontSize: 25 }} />
-            }
-          >
-            View Teachers
-          </Button>
-          <Button
-            component={Link}
-            onClick={() => {
-              navigate("/admin/viewDegrees");
-            }}
-            variant="outlined"
-            color="secondary"
-            startIcon={
-              <WorkspacePremiumIcon color="primary" style={{ fontSize: 25 }} />
-            }
-          >
-            View Degrees
-          </Button>
-        </Box>
-
-        <CardContent style={{ width: "100%" }}>
-          <Typography variant="h5" style={{ marginBottom: "10px" }}>
-            Latest Threads
-          </Typography>
-
-          {threads.map((thread) => (
-            <Card
-              key={thread._id}
-              sx={{
-                ...styles.threadCard,
-                backgroundColor: (theme) => `${theme.palette.primary.main}`,
-                color: "white",
-                position: "relative",
+          {showfall && (
+            <img
+              src={autumn}
+              alt="Fall"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                zIndex: 2,
               }}
+            />
+          )}
+          {showspring && (
+            <img
+              src={spring}
+              alt="Spring"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                zIndex: 2,
+              }}
+            />
+          )}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection={{ xs: "column", md: "row" }}
+            alignItems="center"
+            justifyContent="flex-end"
+            gap={2}
+            sx={{ marginBottom: "20px" }}
+          >
+            <Button
+              component={Link}
+              onClick={() => {
+                navigate("/admin/searchCourses");
+              }}
+              variant="outlined"
+              color="secondary"
+              startIcon={
+                <VisibilityOutlinedIcon
+                  color="primary"
+                  style={{ fontSize: 25 }}
+                />
+              }
             >
-              <CardActionArea onClick={() => handleViewThreadPosts(thread)}>
-                <CardContent>
-                  <Typography sx={({ marginLeft: "10px" }, styles.h7)}>
-                    {thread.title}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
+              View Courses
+            </Button>
+            <Button
+              component={Link}
+              onClick={() => {
+                navigate("/admin/viewStudents");
+              }}
+              variant="outlined"
+              color="secondary"
+              startIcon={<Face2Icon color="primary" style={{ fontSize: 25 }} />}
+            >
+              View Students
+            </Button>
+            <Button
+              component={Link}
+              onClick={() => {
+                navigate("/admin/viewTeachers");
+              }}
+              variant="outlined"
+              color="secondary"
+              startIcon={
+                <PersonSearchIcon color="primary" style={{ fontSize: 25 }} />
+              }
+            >
+              View Teachers
+            </Button>
+            <Button
+              component={Link}
+              onClick={() => {
+                navigate("/admin/viewDegrees");
+              }}
+              variant="outlined"
+              color="secondary"
+              startIcon={
+                <WorkspacePremiumIcon
+                  color="primary"
+                  style={{ fontSize: 25 }}
+                />
+              }
+            >
+              View Degrees
+            </Button>
+          </Box>
 
-              <Box
+          <CardContent style={{ width: "100%" }}>
+            <Typography variant="h5" style={{ marginBottom: "10px" }}>
+              Latest Threads
+            </Typography>
+
+            {threads.map((thread) => (
+              <Card
+                key={thread._id}
                 sx={{
-                  position: "absolute",
-                  top: 0,
-                  right: 30,
-                  bottom: 0,
-                  width: "100px",
+                  ...styles.threadCard,
                   backgroundColor: (theme) => `${theme.palette.primary.main}`,
+                  color: "white",
+                  position: "relative",
                 }}
               >
-                {/* Icon inside the shadow box */}
-                <CampaignOutlinedIcon
-                  className="rotate-icon" // Add a class for easier selection
+                <CardActionArea onClick={() => handleViewThreadPosts(thread)}>
+                  <CardContent>
+                    <Typography sx={({ marginLeft: "10px" }, styles.h7)}>
+                      {thread.title}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+
+                <Box
                   sx={{
-                    fontSize: 60,
-                    color: "white",
                     position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    opacity: "0.9",
-                    transition: "transform 0.3s ease-in-out",
+                    top: 0,
+                    right: 30,
+                    bottom: 0,
+                    width: "100px",
                   }}
-                />
-              </Box>
-            </Card>
-          ))}
-          {threads.length >= 3 && (
-            <Button
-              variant="contained"
-              sx={{ float: "right" }}
-              onClick={() => {
-                navigate("/admin/threads");
-              }}
-            >
-              {" "}
-              View All
-            </Button>
-          )}
-        </CardContent>
-
-        <Card style={{ width: "100%" }}>
-          <CardContent>
-            <Typography variant="h5" style={{ marginBottom: "10px" }}>
-              Semester Schedule
-            </Typography>
-            <Box
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="center"
-            >
+                >
+                  {/* Icon inside the shadow box */}
+                  <CampaignOutlinedIcon
+                    className="rotate-icon" // Add a class for easier selection
+                    sx={{
+                      fontSize: 60,
+                      color: "white",
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                      opacity: "0.9",
+                      transition: "transform 0.3s ease-in-out",
+                    }}
+                  />
+                </Box>
+              </Card>
+            ))}
+            {threads.length >= 3 && (
               <Button
                 variant="contained"
-                onClick={() =>
-                  handleSemesterModalOpen(
-                    <AlarmOnTwoToneIcon
-                      sx={{ fontSize: "4rem", mb: "1rem", color: "primary" }}
-                    />
-                  )
-                }
-                style={{ margin: "10px" }}
+                sx={{ float: "right" }}
+                onClick={() => {
+                  navigate("/admin/threads");
+                }}
               >
-                Start Semester
+                {" "}
+                View All
               </Button>
-              <Button
-                variant="contained"
-                onClick={() =>
-                  handleSemesterEndModalOpen(
-                    <PendingActionsTwoToneIcon
-                      sx={{ fontSize: "4rem", mb: "1rem", color: "primary" }}
-                    />
-                  )
-                }
-                style={{ margin: "10px" }}
-              >
-                End Semester
-              </Button>
-            </Box>
+            )}
           </CardContent>
-        </Card>
-      </div>
 
-      {/* semester modal */}
-      <Dialog open={semesterModalOpen} onClose={handleSemesterModalClose}>
-        <DialogTitle style={{ textAlign: "center" }}>
-          {semesterModalIcon}
-          {semesterModalContent}
-        </DialogTitle>
-        <DialogActions>
-          <Button onClick={handleSemesterModalClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
+          <Card style={{ width: "100%" }}>
+            <CardContent>
+              <Typography variant="h5" style={{ marginBottom: "10px" }}>
+                Semester Schedule
+              </Typography>
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Button
+                  variant="contained"
+                  onClick={() =>
+                    handleSemesterModalOpen(
+                      <AlarmOnTwoToneIcon
+                        sx={{ fontSize: "4rem", mb: "1rem", color: "primary" }}
+                      />
+                    )
+                  }
+                  style={{ margin: "10px" }}
+                >
+                  Start Semester
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() =>
+                    handleSemesterEndModalOpen(
+                      <PendingActionsTwoToneIcon
+                        sx={{ fontSize: "4rem", mb: "1rem", color: "primary" }}
+                      />
+                    )
+                  }
+                  style={{ margin: "10px" }}
+                >
+                  End Semester
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </div>
+
+        {/* semester modal */}
+        <Dialog open={semesterModalOpen} onClose={handleSemesterModalClose}>
+          <DialogTitle style={{ textAlign: "center" }}>
+            {semesterModalIcon}
+            {semesterModalContent}
+          </DialogTitle>
+          <DialogActions>
+            <Button onClick={handleSemesterModalClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
       </Container>
       <Dialog open={downloading}>
         <Box
